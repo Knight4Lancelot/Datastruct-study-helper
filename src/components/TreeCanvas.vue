d0b83f26fd446ba27951b8dc5e44963ad0844e4a
Knight4Lancelot <320062713@qq.com>
2022-06-17 19:04:56 +0800

    初始化项目

---
+++ src/components/TreeCanvas.vue
@@ -0,0 +1,14 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+  name: 'TreeCanvas',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <treecanvas></treecanvas>
+  </div>
+</template>
+
+<script>
+import treecanvas from '../components/TreeCanvas.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+	treecanvas
+  }
+}
+</script>
