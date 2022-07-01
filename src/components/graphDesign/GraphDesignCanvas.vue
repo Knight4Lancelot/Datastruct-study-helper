91301159f00ade65cef44ce13723e24234c6f38a
Knight4Lancelot <320062713@qq.com>
2022-07-01 11:25:50 +0800

    2022/7/1修改

---
+++ src/components/graphDesign/GraphDesignCanvas.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class='graph-canvas-component'>
+		<div class="canvas-graph-node">
+			<graphnode
+				ref="graphnodeElement"
+				v-for="(stn, k) in nodeArray"
+				:key="k"
+				:index="stn.mapIndex"
+				class="graph-node-list"
+				:valElement="stn.val">
+			</graphnode>
+		</div>
+		<canvas id="canvas-graph-edge"/>
+	</div>
+</template>
+
+<script>
+import graphnode from './GraphDesignNode.vue'
+
+export default {
+  name: 'graphCanvas',
+  components: {
+	graphnode
+  },
+  data() {
+	return {
+		nodecount: 0,
+		nodeArray: [  ]
+	}
+  },
+}
+</script>
+
+<style>
+.graph-canvas-component {
+	margin: 5%;
+	width: 90%;
+	height: 100%;
+	overflow: hidden;
+}
+</style>
--- src/views/DesignGraphView.vue
+++ src/views/DesignGraphView.vue
@@ -1,11 +1,136 @@
 <template>
-	<div>
-		设计图
+	<div class="designgraph">
+		<router-graph to="/" exact><i id="back-to-home" class="el-icon-arrow-left"/></router-graph>
+		<span id="design-graph-headword"
+			:style="{'top':'55px',
+					'left':'120px'}">
+			<span style="color:#67C23A;font-size:35px;font-weight: 800;">
+				图&nbsp;
+			</span>结构设计
+		</span>
+		<graphcanvas class="graph-canvas-show"
+			id="graph-canvas-show"
+			:elementList="graphList"
+			v-if="isshowCanvas"
+			ref="canvasElement"
+			:style="{'overflow-y':'scroll', 'overflow-x':'scroll',
+			'max-width': String(canvasWidth)+'px',
+			'max-height': String(canvasHeight)+'px',
+			'width': String(canvasWidth)+'px',
+			'height': String(canvasHeight)+'px'}"/>
+		<span id="tree-result-word"
+			:style="{'width': String(canvasWidth)+'px',
+				'top':String(canvasHeight+240)+'px',
+				'left':'120px'}">
+				<span style="color:#67C23A;font-size:35px;font-weight: 800;">
+					图&nbsp;
+				</span>的数组形式表示结果
+		</span>
+		<el-card id="graph-result-card"
+			:style="{'width': String(canvasWidth)+'px',
+					'padding':'0', 'margin':'0',
+					'top':String(canvasHeight+310)+'px',
+					'left':'120px'}">
+			<div id="card-message" v-text="holdResString" @click="sendDataCopy()"/>
+		</el-card>
+		<span id="graph-result-code"
+			:style="{'width': String(canvasWidth)+'px',
+				'top':String(canvasHeight+480)+'px',
+				'left':'120px'}">
+				<span style="color:#67C23A;font-size:35px;font-weight: 800;">
+					图&nbsp;
+				</span>代码展示
+		</span>
+		<div id="code-view"
+			:style="{'top':String(canvasHeight+550)+'px',
+				'width':String(canvasWidth-30)+'px',
+				'max-width':String(canvasWidth-30)+'px'
+				}">
+			<button :class="codeClass[0]" @click="changeLanguage(1)">C++</button>
+			<button :class="codeClass[1]" @click="changeLanguage(2)">Java</button>
+			<button :class="codeClass[2]" @click="changeLanguage(3)">Python</button>
+			<prism-editor
+				class="my-editor height-200"
+				v-model="code"
+				:highlight="highlighter"
+				line-numbers
+				readonly></prism-editor>
+		</div>
+		<div id="page-bottom"
+			:style="{'width': String(canvasWidth-30)+'px',
+				'top':String(canvasHeight+1390)+'px'}">
+			已经到页面底部了
+		</div>
 	</div>
 </template>
 
 <script>
+import graphcanvas from '../components/graphDesign/GraphDesignCanvas.vue'
+
+import { PrismEditor } from 'vue-prism-editor';
+import 'vue-prism-editor/dist/prismeditor.min.css';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism-tomorrow.css';
+const durationTime=5
+
+export default {
+  name: 'GraphDesignHomeView',
+  components: {
+	graphcanvas,
+	PrismEditor
+  },
+  data() {
+	return {
+		code: '',
+		holdResString: '',
+		canvasHeight: 0,
+		canvasWidth: 0,
+		appHeight: 0,
+		appWidth: 0,
+		isshowCanvas: true,
+		graphList: [ '1', '2', '3' ],
+		codeClass: [ "code-language-active", "code-language", "code-language" ]
+	}
+  },
+  mounted() {
+	this.formCanvasSize()
+  },
+  methods: {
+	highlighter(code) {
+		return highlight(this.code, languages.js, "js");
+	},
+	formCanvasSize() {
+		this.appHeight=document.documentElement.clientHeight
+		this.appWidth=document.documentElement.clientWidth
+		this.canvasHeight = this.appHeight-160
+		this.canvasWidth = this.appWidth-300
+		if (this.canvasHeight < 500) { this.canvasHeight = 500 }
+		if (this.canvasWidth < 1000) { this.canvasWidth = 1000 }
+		// this.$refs['canvasElement'].draw()
+	}
+  }
+}
 </script>
 
 <style>
+.designgraph {
+	width: 100%;
+}
+#design-graph-headword, #graph-result-word, #graph-result-code {
+	position: absolute;
+	font-size: 25px;
+	color: #6A6A6A;
+	font-family: 'Microsoft YaHei';
+	user-select: none;
+}
+.graph-canvas-show {
+	position: absolute;
+	top: 150px;
+	left: 120px;
+	margin: 0;
+	border: 2px solid #C0C4CC;
+	border-radius: 15px;
+}
 </style>
